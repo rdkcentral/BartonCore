cf3c69cda507a9bfb48155f05050435cca7e021f
kfundecmcsa <kevin_funderburg@comcast.com>
2024-11-12 15:33:25 -0600

    ci: include output-on-failure for ctest (#74)
    - Include the `--output-on-failure` option to ensure logging occurs on a
      ctest failure.
    
    - Remove sample build/matter/out directory after each sample app is
      built to prevent sample apps from piling on top of each other and
      using too much space on the github runner.

--- build-matter.sh
+++ build-matter.sh
@@ -42,11 +42,11 @@ mkdir -p ${MATTER_INSTALL_BIN_DIR}
 export TMPDIR=${BUILD_DIR}/tmp
 export TERM=xterm
 . ./scripts/activate.sh
-./scripts/build/build_examples.py --target linux-x64-light-rpc build && cp out/linux-x64-light-rpc/chip-lighting-app ${MATTER_INSTALL_BIN_DIR}
-./scripts/build/build_examples.py --target linux-x64-lock build && cp out/linux-x64-lock/chip-lock-app ${MATTER_INSTALL_BIN_DIR}
-./scripts/build/build_examples.py --target linux-x64-thermostat build && cp out/linux-x64-thermostat/thermostat-app ${MATTER_INSTALL_BIN_DIR}
-./scripts/build/build_examples.py --target linux-x64-chip-tool build && cp out/linux-x64-chip-tool/chip-tool ${MATTER_INSTALL_BIN_DIR}
+
+./scripts/build/build_examples.py --target linux-x64-light-rpc build && cp out/linux-x64-light-rpc/chip-lighting-app ${MATTER_INSTALL_BIN_DIR} && rm -rf out
+./scripts/build/build_examples.py --target linux-x64-lock build && cp out/linux-x64-lock/chip-lock-app ${MATTER_INSTALL_BIN_DIR} && rm -rf out
+./scripts/build/build_examples.py --target linux-x64-thermostat build && cp out/linux-x64-thermostat/thermostat-app ${MATTER_INSTALL_BIN_DIR} && rm -rf out
+./scripts/build/build_examples.py --target linux-x64-chip-tool build && cp out/linux-x64-chip-tool/chip-tool ${MATTER_INSTALL_BIN_DIR} && rm -rf out
 
 # Clean up
-rm -rf out
 rm -rf .environment
