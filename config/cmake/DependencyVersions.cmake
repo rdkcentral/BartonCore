rdkcentral/BartonCore

commit 908d8ab4625a4377918dc44e23e9402452ffa0bc

build: Add BCoreMatterHelper (#48)
This commit provides a new cmake module to better interface to select
and query for Matter providers and delegates. Currently, this
implementations sits on top of the existing public options. This is
because these options must exist still to allow clients a mechanism to
select these providers/delegates via command-line arguments. As a
result, the implementation of this module modifies these cache variables
forcibly. For most places (including initial cache files), this is fine
and you can freely use the module. For options.cmake, it is still
required to define the defaults using bds_string_option for transparancy
of defaults as well as for correct options dump in the cmake output. In
addition, you would not want these defaults to force over client
selected ones.

config/cmake/options.cmake
@@ -204,6 +204,11 @@ bcore_string_option(NAME BCORE_MATTER_LIB
                   DESCRIPTION "Name of the provided Matter library."
                   VALUE "BartonMatter")
 
+# We're not going to use BCoreMatterHelper to set up defaults (as BCoreMatterHelper applies FORCE
+# sets). However, include it to populate default header paths.
+# Default selected implementations still need to be set up in below lists.
+include(BCoreMatterHelper)
+
 set(MATTER_PROVIDER_DELEGATE_PARENT_DIR "${PROJECT_SOURCE_DIR}/core/src/subsystems/matter")
 set(MATTER_PROVIDER_DEFAULT_DIR "${MATTER_PROVIDER_DELEGATE_PARENT_DIR}/providers/default")
 set(MATTER_DELEGATE_DEFAULT_DIR "${MATTER_PROVIDER_DELEGATE_PARENT_DIR}/delegates/default")
@@ -221,15 +226,11 @@ bcore_string_option(NAME BCORE_MATTER_DELEGATE_IMPLEMENTATIONS
 
 bcore_string_option(NAME BCORE_MATTER_PROVIDER_HEADER_PATHS
                   DEFINITION BARTON_CONFIG_MATTER_PROVIDER_HEADER_PATHS
-                  DESCRIPTION "List of paths to directories containing matter provider header files."
-                  VALUE "${MATTER_PROVIDER_DELEGATE_PARENT_DIR}/providers"
-                        "${MATTER_PROVIDER_DEFAULT_DIR}")
+                  DESCRIPTION "List of paths to directories containing matter provider header files.")
 
 bcore_string_option(NAME BCORE_MATTER_DELEGATE_HEADER_PATHS
                   DEFINITION BARTON_CONFIG_MATTER_DELEGATE_HEADER_PATHS
-                  DESCRIPTION "List of paths to directories containing matter delegate header files"
-                  VALUE "${MATTER_PROVIDER_DELEGATE_PARENT_DIR}/delegates"
-                        "${MATTER_DELEGATE_DEFAULT_DIR}")
+                  DESCRIPTION "List of paths to directories containing matter delegate header files")
 
 bcore_string_option(NAME BCORE_MATTER_BLE_CONTROLLER_DEVICE_NAME
                     DEFINITION BARTON_CONFIG_MATTER_BLE_CONTROLLER_DEVICE_NAME

core/CMakeLists.txt
@@ -72,25 +72,34 @@ if (BCORE_ZIGBEE)
 endif() #BARTON_CONFIG_ZIGBEE
 
 if (BCORE_MATTER)
+    include(BCoreMatterHelper)
+
     # Check for jsoncpp here rather than in DependencyVersions.cmake because the
     # Matter build process installs jsoncpp, and might not be available during
     # initial dependency resolution
     set(JSONCPP_MIN_VERSION 1.9.5)
 
     file(GLOB matterSubSrc "src/subsystems/matter/*.cpp")
-    file(GLOB matterSubProviderCommonSrc "src/subsystems/matter/delegates/*.cpp" "src/subsystems/matter/providers/*.cpp")
 
     file(GLOB matterDriversSrc "deviceDrivers/matter/*.cpp" "deviceDrivers/matter/clusters/*.cpp" "deviceDrivers/matter/subscriptions/*.cpp")
 
     add_compile_definitions(CHIP_HAVE_CONFIG_H=1)
 
+    bcore_matter_helper_provider_get_implementations(
+        OUTPUT MATTER_PROVIDER_IMPLEMENTATIONS
+    )
+
+    bcore_matter_helper_delegate_get_implementations(
+        OUTPUT MATTER_DELEGATE_IMPLEMENTATIONS
+    )
+
     list(APPEND SOURCES
         ${matterSubSrc}
-        ${matterSubProviderCommonSrc}
-        ${BCORE_MATTER_PROVIDER_IMPLEMENTATIONS}
-        ${BCORE_MATTER_DELEGATE_IMPLEMENTATIONS}
+        ${MATTER_PROVIDER_IMPLEMENTATIONS}
+        ${MATTER_DELEGATE_IMPLEMENTATIONS}
         ${matterDriversSrc}
         ${matterSrc}
+        "src/subsystems/matter/providers/BartonDeviceInstanceInfoProvider.cpp" # This provider is always required
     )
 
     # BartonMatter depends on openSSL and glib.
@@ -116,10 +125,18 @@ if (BCORE_MATTER)
 
     bcore_find_package(NAME jsoncpp MIN_VERSION ${JSONCPP_MIN_VERSION} REQUIRED)
 
+    bcore_matter_helper_provider_get_header_paths(
+        OUTPUT MATTER_PROVIDER_HEADER_PATHS
+    )
+
+    bcore_matter_helper_delegate_get_header_paths(
+        OUTPUT MATTER_DELEGATE_HEADER_PATHS
+    )
+
     list(APPEND XTRA_INCLUDES
         ${OPENSSL_INCLUDE_DIRS}
-        ${BCORE_MATTER_PROVIDER_HEADER_PATHS}
-        ${BCORE_MATTER_DELEGATE_HEADER_PATHS})
+        ${MATTER_PROVIDER_HEADER_PATHS}
+        ${MATTER_DELEGATE_HEADER_PATHS})
 
     list(APPEND XTRA_LIBS ${BCORE_MATTER_LIB} ${OPENSSL_LINK_LIBRARIES} certifier jsoncpp)
     link_directories(${CMAKE_BINARY_DIR}/matter-install/lib)

config/cmake/platforms/dev/linux.cmake
@@ -33,24 +33,15 @@ set(BCORE_BUILD_WITH_ASAN ON CACHE BOOL "Build with ASAN")
 set(CMAKE_PREFIX_PATH "${CMAKE_BINARY_DIR}/matter-install" CACHE PATH "Path to Matter installation")
 
 # Matter settings
+include(${CMAKE_SOURCE_DIR}/config/cmake/modules/BCoreMatterHelper.cmake)
+
 set(BCORE_MATTER_USE_RANDOM_PORT ON CACHE BOOL "Use random port")
-set(BCORE_MATTER_DELEGATE_IMPLEMENTATIONS
-    "${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/delegates/dev/SelfSignedCertifierOperationalCredentialsIssuer.cpp"
-    CACHE
-    STRING
-    "Delegate implementations")
-set(BCORE_MATTER_DELEGATE_HEADER_PATHS
-    "${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/delegates;${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/delegates/dev"
-    CACHE
-    STRING
-    "Delegate header paths")
-set(BCORE_MATTER_PROVIDER_IMPLEMENTATIONS
-    "${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/providers/dev/BartonTestDACProvider.cpp;${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/providers/default/DefaultCommissionableDataProvider.cpp"
-    CACHE
-    STRING
-    "Provider implementations")
-set(BCORE_MATTER_PROVIDER_HEADER_PATHS
-    "${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/providers;${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/providers/dev;${CMAKE_SOURCE_DIR}/core/src/subsystems/matter/providers/default"
-    CACHE
-    STRING
-    "Provider header paths")
+
+bcore_matter_helper_delegate_add_implementation(
+    DEV SelfSignedCertifierOperationalCredentialsIssuer
+)
+
+bcore_matter_helper_provider_add_implementation(
+    DEFAULT DefaultCommissionableDataProvider
+    DEV BartonTestDACProvider
+)

commit 21806224a2a08599e9b5b3921e5b9ef1b037dd25

build!: Remove CertifierDACProvider.cpp
This commit removes CertifierDACProvider.cpp as it did not pan out how
we initially thought and no existing client needed the implementation.
For now:

1. options.cmake defaults to using the BartonTestDACProvider (which is a
   thin wrapper around the SDK test DAC provider). Clients are expected
   to implement their own real DAC providers.

2. A symlink has been created in
   core/src/subsystems/matter/providers/default to the
   BartonTestDACProvider in dev/. This is just for clarity about what
   the default is under this system of matter plugin code. This may
   change if we overhaul the plugin system.

3. A bug fix was added to options.cmake for cases where you build with
   pure defaults. In this case, it could previously not select the
   BartonCommissionableDataProvider. This regression was introduced in
   908d8ab4625a4377918dc44e23e9402452ffa0bc.

BREAKING CHANGE: As CertifierDACProvider.cpp was removed, this may be
disruptive to clients, causing changes to how they build BartonCore.

config/cmake/DependencyVersions.cmake
@@ -38,7 +38,6 @@ set(OPENSSL_MAX_VERSION 1.1.1v)
 set(UUID_MIN_VERSION 1.0.3)
 set(XML_MIN_VERSION 2.9.8)
 set(MBEDCRYPTO_MIN_VERSION 2.28.4)
-set(LIBCERTIFIER_MIN_VERSION 2.2.2)
 
 # We depend on dbus client library and some headers but versioning may not
 # exist for those, so we'll just let the linker find it and pray it
@@ -61,7 +60,3 @@ bcore_find_package(NAME libssl MIN_VERSION ${OPENSSL_MIN_VERSION} MAX_VERSION ${
 bcore_find_package(NAME uuid MIN_VERSION ${UUID_MIN_VERSION} REQUIRED)
 bcore_find_package(NAME libxml-2.0 MIN_VERSION ${XML_MIN_VERSION} REQUIRED)
 bcore_find_package(NAME mbedcrypto MIN_VERSION ${MBEDCRYPTO_MIN_VERSION} REQUIRED)
-
-if (BCORE_MATTER)
-    bcore_find_package(NAME certifier MIN_VERSION ${LIBCERTIFIER_MIN_VERSION} REQUIRED)
-endif()

config/cmake/options.cmake
@@ -215,7 +215,8 @@ set(MATTER_DELEGATE_DEFAULT_DIR "${MATTER_PROVIDER_DELEGATE_PARENT_DIR}/delegate
 bcore_string_option(NAME BCORE_MATTER_PROVIDER_IMPLEMENTATIONS
                   DEFINITION BARTON_CONFIG_MATTER_PROVIDER_IMPLEMENTATIONS
                   DESCRIPTION "List of paths to source files that implement Matter provider interfaces."
-                  VALUE "${MATTER_PROVIDER_DEFAULT_DIR}/CertifierDACProvider.cpp"
+                  VALUE "${MATTER_PROVIDER_DEFAULT_DIR}/BartonTestDACProvider.cpp"
+                        "${MATTER_PROVIDER_DELEGATE_PARENT_DIR}/providers/BartonCommissionableDataProvider.cpp"
                         "${MATTER_PROVIDER_DEFAULT_DIR}/DefaultCommissionableDataProvider.cpp")
 
 bcore_string_option(NAME BCORE_MATTER_DELEGATE_IMPLEMENTATIONS

core/CMakeLists.txt
@@ -138,7 +138,7 @@ if (BCORE_MATTER)
         ${MATTER_PROVIDER_HEADER_PATHS}
         ${MATTER_DELEGATE_HEADER_PATHS})
 
-    list(APPEND XTRA_LIBS ${BCORE_MATTER_LIB} ${OPENSSL_LINK_LIBRARIES} certifier jsoncpp)
+    list(APPEND XTRA_LIBS ${BCORE_MATTER_LIB} ${OPENSSL_LINK_LIBRARIES} jsoncpp)
     link_directories(${CMAKE_BINARY_DIR}/matter-install/lib)
 endif()
 

core/src/subsystems/matter/providers/default/BartonTestDACProvider.cpp
@@ -1,0 +1,1 @@
+../dev/BartonTestDACProvider.cpp

core/src/subsystems/matter/providers/default/CertifierDACProvider.cpp
@@ -1,191 +1,0 @@
-// ------------------------------ tabstop = 4 ----------------------------------
-//
-// If not stated otherwise in this file or this component's LICENSE file the
-// following copyright and licenses apply:
-//
-// Copyright 2021 Comcast Cable Communications Management, LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-// ------------------------------ tabstop = 4 ----------------------------------
-
-#include "CertifierDACProvider.hpp"
-
-#include <crypto/CHIPCryptoPAL.h>
-
-#include <lib/core/CHIPError.h>
-#include <lib/support/CodeUtils.h>
-#include <lib/support/Span.h>
-
-#include <certifier/security.h>
-#include <memory>
-
-#include "BartonMatterProviderRegistry.hpp"
-
-extern "C" {
-#include "deviceServiceConfiguration.h"
-#include "deviceServiceProps.h"
-#include "icLog/logging.h"
-#include "provider/barton-core-property-provider.h"
-}
-
-#define LOG_TAG     "CertifierDACProvider"
-#define logFmt(fmt) "(%s): " fmt, __func__
-
-using namespace barton;
-
-namespace {
-    CHIP_ERROR LoadKeypairFromRaw(ByteSpan private_key, ByteSpan public_key, Crypto::P256Keypair & keypair)
-    {
-        Crypto::P256SerializedKeypair serialized_keypair;
-        ReturnErrorOnFailure(serialized_keypair.SetLength(private_key.size() + public_key.size()));
-        memcpy(serialized_keypair.Bytes(), public_key.data(), public_key.size());
-        memcpy(serialized_keypair.Bytes() + public_key.size(), private_key.data(), private_key.size());
-        return keypair.Deserialize(serialized_keypair);
-    }
-
-    std::shared_ptr<CertifierDACProvider> dacProvider = std::make_shared<CertifierDACProvider>();
-    auto providerRegistered = BartonMatterProviderRegistry::Instance().RegisterBartonDACProvider(dacProvider);
-}
-
-CHIP_ERROR CertifierDACProvider::GetDeviceAttestationCert(MutableByteSpan & out_dac_buffer)
-{
-    X509_CERT * cert     = nullptr;
-    CertifierError error = CERTIFIER_ERROR_INITIALIZER;
-
-    std::string dacFilePath = GetDACFilepath();
-    std::string dacPassword = GetDACPassword();
-    error = security_get_X509_PKCS12_file(dacFilePath.c_str(), dacPassword.c_str(), nullptr, &cert, nullptr);
-    VerifyOrReturnError(error.application_error_code == 0 && error.library_error_code == 0, CHIP_ERROR_INTERNAL);
-
-    size_t der_len      = 0;
-    unsigned char * der = security_X509_to_DER(cert, &der_len);
-    VerifyOrReturnError(der != nullptr, CHIP_ERROR_INTERNAL);
-
-    CopySpanToMutableSpan(ByteSpan(der, der_len), out_dac_buffer);
-
-    XFREE(der);
-    security_free_cert(cert);
-
-    return CHIP_NO_ERROR;
-}
-
-CHIP_ERROR CertifierDACProvider::GetProductAttestationIntermediateCert(MutableByteSpan & out_pai_buffer)
-{
-    X509_LIST * certs    = nullptr;
-    X509_CERT * cert     = nullptr;
-    CertifierError error = CERTIFIER_ERROR_INITIALIZER;
-
-    certs = security_new_cert_list();
-    VerifyOrReturnError(certs != nullptr, CHIP_ERROR_INTERNAL);
-
-    std::string dacFilePath = GetDACFilepath();
-    std::string dacPassword = GetDACPassword();
-    error = security_get_X509_PKCS12_file(dacFilePath.c_str(), dacPassword.c_str(), certs, nullptr, nullptr);
-    VerifyOrReturnError(error.application_error_code == 0 && error.library_error_code == 0, CHIP_ERROR_INTERNAL);
-
-    cert = security_cert_list_get(certs, 1);
-
-    size_t der_len      = 0;
-    unsigned char * der = security_X509_to_DER(cert, &der_len);
-    VerifyOrReturnError(der != nullptr, CHIP_ERROR_INTERNAL);
-
-    CopySpanToMutableSpan(ByteSpan(der, der_len), out_pai_buffer);
-
-    XFREE(der);
-    security_free_cert(cert);
-    security_free_cert_list(certs);
-
-    return CHIP_NO_ERROR;
-}
-
-CHIP_ERROR CertifierDACProvider::GetCertificationDeclaration(MutableByteSpan & out_cd_buffer)
-{
-    // TODO: We don't yet have CMS for CDs (issued by matter upon certification - Matter spec 1.4 section 6.3)
-    out_cd_buffer.reduce_size(0);
-
-    return CHIP_NO_ERROR;
-}
-
-CHIP_ERROR CertifierDACProvider::GetFirmwareInformation(MutableByteSpan & out_firmware_info_buffer)
-{
-    // TODO: We need a real example FirmwareInformation to be populated.
-    out_firmware_info_buffer.reduce_size(0);
-
-    return CHIP_NO_ERROR;
-}
-
-CHIP_ERROR CertifierDACProvider::SignWithDeviceAttestationKey(const ByteSpan & message_to_sign,
-                                                              MutableByteSpan & out_signature_buffer)
-{
-    Crypto::P256ECDSASignature signature;
-    Crypto::P256Keypair keypair;
-
-    VerifyOrReturnError(!out_signature_buffer.empty(), CHIP_ERROR_INVALID_ARGUMENT);
-    VerifyOrReturnError(!message_to_sign.empty(), CHIP_ERROR_INVALID_ARGUMENT);
-    VerifyOrReturnError(out_signature_buffer.size() >= signature.Capacity(), CHIP_ERROR_BUFFER_TOO_SMALL);
-
-    X509_CERT * cert     = nullptr;
-    ECC_KEY * key        = nullptr;
-    CertifierError error = CERTIFIER_ERROR_INITIALIZER;
-
-    std::string dacFilePath = GetDACFilepath();
-    std::string dacPassword = GetDACPassword();
-    error = security_get_X509_PKCS12_file(dacFilePath.c_str(), dacPassword.c_str(), nullptr, &cert, &key);
-    VerifyOrReturnError(error.application_error_code == 0 && error.library_error_code == 0, CHIP_ERROR_INTERNAL);
-
-    uint8_t raw_public_key[65]  = { 0 };
-    uint8_t raw_private_key[32] = { 0 };
-    size_t raw_public_key_len   = security_serialize_raw_public_key(key, raw_public_key, sizeof(raw_public_key));
-    size_t raw_private_key_len  = security_serialize_raw_private_key(key, raw_private_key, sizeof(raw_private_key));
-    VerifyOrReturnError(raw_public_key_len == sizeof(raw_public_key), CHIP_ERROR_INTERNAL);
-    VerifyOrReturnError(raw_private_key_len == sizeof(raw_private_key), CHIP_ERROR_INTERNAL);
-
-    ReturnErrorOnFailure(LoadKeypairFromRaw(ByteSpan{ raw_private_key, raw_private_key_len },
-                                            ByteSpan{ raw_public_key, raw_public_key_len }, keypair));
-
-    security_free_eckey(key);
-    security_free_cert(cert);
-
-    ReturnErrorOnFailure(keypair.ECDSA_sign_msg(message_to_sign.data(), message_to_sign.size(), signature));
-
-    return CopySpanToMutableSpan(ByteSpan{ signature.ConstBytes(), signature.Length() }, out_signature_buffer);
-}
-
-std::string CertifierDACProvider::GetDACFilepath()
-{
-    g_autoptr(BCorePropertyProvider) propertyProvider = deviceServiceConfigurationGetPropertyProvider();
-    g_autofree char *dacFilePath = b_core_property_provider_get_property_as_string(propertyProvider, DEVICE_PROP_MATTER_DAC_P12_PATH,  "");
-
-    if (dacFilePath == nullptr || strlen(dacFilePath) == 0)
-    {
-        icWarn("Device Attestation Certificate file path is not set.");
-    }
-
-    return std::string(dacFilePath);
-}
-
-std::string CertifierDACProvider::GetDACPassword()
-{
-    g_autoptr(BCorePropertyProvider) propertyProvider = deviceServiceConfigurationGetPropertyProvider();
-    g_autofree char *dacPassword = b_core_property_provider_get_property_as_string(propertyProvider, DEVICE_PROP_MATTER_DAC_P12_PASSWORD, "");
-
-    if (dacPassword == nullptr || strlen(dacPassword) == 0)
-    {
-        icWarn("Device Attestation Certificate password is not set.");
-    }
-
-    return std::string(dacPassword);
-}

core/src/subsystems/matter/providers/default/CertifierDACProvider.hpp
@@ -1,52 +1,0 @@
-// ------------------------------ tabstop = 4 ----------------------------------
-//
-// If not stated otherwise in this file or this component's LICENSE file the
-// following copyright and licenses apply:
-//
-// Copyright 2021 Comcast Cable Communications Management, LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-// ------------------------------ tabstop = 4 ----------------------------------
-
-#pragma once
-
-#include <credentials/DeviceAttestationCredsProvider.h>
-#include <lib/core/Optional.h>
-#include <string>
-
-using namespace chip;
-using namespace Credentials;
-
-namespace barton {
-
-class CertifierDACProvider : public DeviceAttestationCredentialsProvider
-{
-public:
-    CertifierDACProvider() = default;
-    ~CertifierDACProvider() override = default;
-
-    CHIP_ERROR GetCertificationDeclaration(MutableByteSpan & out_cd_buffer) override;
-    CHIP_ERROR GetFirmwareInformation(MutableByteSpan & out_firmware_info_buffer) override;
-    CHIP_ERROR GetDeviceAttestationCert(MutableByteSpan & out_dac_buffer) override;
-    CHIP_ERROR GetProductAttestationIntermediateCert(MutableByteSpan & out_pai_buffer) override;
-    CHIP_ERROR SignWithDeviceAttestationKey(const ByteSpan & message_to_sign, MutableByteSpan & out_signature_buffer) override;
-
-private:
-    std::string GetDACFilepath();
-    std::string GetDACPassword();
-};
-
-} // namespace barton