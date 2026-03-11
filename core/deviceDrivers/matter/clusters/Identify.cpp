cb045f09fa67cf4a319d9c3bfd90e4c20f6c85e0
rchowd807 <Raiyan_Chowdhury@comcast.com>
2026-03-11 09:29:27 -0500

    fix(matter): Fix attribute reporting for common clusters
    Fix the handling of attribute reports for common clusters not
    specified in the SBMD files, which was broken during the switch
    to SBMD.
    Move the IdentifyTime attribute out of the SBMD files and into a
    C++ MatterCluster implementation for the Identify cluster, as it is
    a common cluster present in most device types.
    Write a Python test that tests Barton's handling of one common cluster
    attribute (IdentifyTime).
    
    Refs: BARTON-348

---
+++ core/deviceDrivers/matter/clusters/Identify.cpp
@@ -0,0 +1,78 @@
+// ------------------------------ tabstop = 4 ----------------------------------
+//
+// If not stated otherwise in this file or this component's LICENSE file the
+// following copyright and licenses apply:
+//
+// Copyright 2025 Comcast Cable Communications Management, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// ------------------------------ tabstop = 4 ----------------------------------
+
+//
+// Created by Raiyan Chowdhury on 3/4/26.
+//
+
+#define LOG_TAG     "IdentifyCluster"
+#define logFmt(fmt) "(%s): " fmt, __func__
+
+extern "C" {
+#include <icLog/logging.h>
+}
+
+#include "Identify.hpp"
+#include "app/ConcreteAttributePath.h"
+#include "lib/core/CHIPError.h"
+
+using namespace chip::app::Clusters::Identify;
+
+
+namespace barton
+{
+    void Identify::OnAttributeChanged(chip::app::ClusterStateCache *cache,
+                                     const chip::app::ConcreteAttributePath &path)
+    {
+        using namespace chip::app;
+
+        if (path.mEndpointId != endpointId || path.mClusterId != Clusters::Identify::Id)
+        {
+            return;
+        }
+
+        switch (path.mAttributeId)
+        {
+            case Attributes::IdentifyTime::Id:
+            {
+                using TypeInfo = Attributes::IdentifyTime::TypeInfo;
+
+                TypeInfo::DecodableType value;
+                CHIP_ERROR error = cache->Get<TypeInfo>(path, value);
+                if (error == CHIP_NO_ERROR)
+                {
+                    static_cast<Identify::EventHandler *>(eventHandler)->IdentifyTimeChanged(deviceId, value);
+                }
+                else
+                {
+                    icError("Failed to read IdentifyTime attribute for device %s: %s", deviceId.c_str(),
+                            error.AsString());
+                }
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+} // namespace barton
